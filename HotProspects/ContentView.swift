8af2afacd00482a5328d03299e1f90e9bcfac503
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-09-27 03:52:01 -0400

    Initial Commit

---
+++ HotProspects/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Jordan Haynes on 9/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Jordan Haynes on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
